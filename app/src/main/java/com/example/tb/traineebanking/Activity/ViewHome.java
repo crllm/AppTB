35b1afa0ca8355fa97bdd4f8139d6bc6cc991d20
Guilherme Alves <gui.jfalves@gmail.com>
2018-11-30 17:39:50 -0200

    test

--- app/src/main/java/com/example/tb/traineebanking/Activity/ViewHome.java
+++ app/src/main/java/com/example/tb/traineebanking/Activity/ViewHome.java
@@ -11,5 +11,6 @@ public class ViewHome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_home);
+        int i = 5;
     }
 }
