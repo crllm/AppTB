6cd3e35ec940974e64766ff6228d3d1bc9075b1f
Guilherme Alves <gui.jfalves@gmail.com>
2018-12-03 13:58:18 -0200

    view pagamento + borders

---
+++ app/src/main/java/com/example/tb/traineebanking/Activity/ViewEmprestimo.java
@@ -0,0 +1,13 @@
+package com.example.tb.traineebanking.Activity;
+import com.example.tb.traineebanking.R;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewEmprestimo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_emprestimo);
+    }
+}
