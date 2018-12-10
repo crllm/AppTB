d63916c9e1d7d98d0e742d89da54bf668165988a
Rafael Borduchi Amoroso <rafa_borduchi@hotmail.com>
2018-12-10 14:47:59 -0200

    Getters e Setter do Investimento

--- app/src/main/java/com/example/tb/traineebanking/Models/Investimento.java
+++ app/src/main/java/com/example/tb/traineebanking/Models/Investimento.java
@@ -40,5 +40,77 @@ public class Investimento implements Serializable {
     @SerializedName("Status")
     public int status;
 
+    public Investimento() {}
 
+    public int getIdInvestimento() {
+        return idInvestimento;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public Date getDataInvestimento() {
+        return dataInvestimento;
+    }
+
+    public Conta getConta() {
+        return conta;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public double getJuros() {
+        return juros;
+    }
+
+    public double getMulta() {
+        return multa;
+    }
+
+    public Date getDataTermino() {
+        return dataTermino;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setIdInvestimento(int idInvestimento) {
+        this.idInvestimento = idInvestimento;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public void setDataInvestimento(Date dataInvestimento) {
+        this.dataInvestimento = dataInvestimento;
+    }
+
+    public void setConta(Conta conta) {
+        this.conta = conta;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setJuros(double juros) {
+        this.juros = juros;
+    }
+
+    public void setMulta(double multa) {
+        this.multa = multa;
+    }
+
+    public void setDataTermino(Date dataTermino) {
+        this.dataTermino = dataTermino;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
 }
