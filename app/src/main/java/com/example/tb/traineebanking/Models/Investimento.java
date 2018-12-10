34cbd9d2e917d2aa9f9c2cd68616645f8374c4ef
crllm <caarolmantovani@hotmail.com>
2018-12-10 14:43:43 -0200

    Investimento

--- app/src/main/java/com/example/tb/traineebanking/Models/Investimento.java
+++ app/src/main/java/com/example/tb/traineebanking/Models/Investimento.java
@@ -1,23 +1,44 @@
 package com.example.tb.traineebanking.Models;
 
+import com.fasterxml.jackson.annotation.JsonRootName;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
 import java.util.Date;
 
 /**
  * Created by rafa_ on 04/12/2018.
  */
+@JsonRootName("Investimento")
 
-public class Investimento {
-    private int idInvestimento;
-    private double valor;
-    private Date dataInvestimento;
-    private Conta conta;
-    private String tipo;
-    private double juros;
-    private double multa;
-    private Date dataTermino;
-    private int status;
+public class Investimento implements Serializable {
 
-    public Investimento() {
+    @SerializedName("IdInvestimento")
+    public int idInvestimento;
 
-    }
+    @SerializedName("Valor")
+    public double valor;
+
+    @SerializedName("DataInvestimento")
+    public Date dataInvestimento;
+
+    @SerializedName("Conta")
+    public Conta conta;
+
+    @SerializedName("Tipo")
+    public String tipo;
+
+    @SerializedName("Juros")
+    public double juros;
+
+    @SerializedName("Multa")
+    public double multa;
+
+    @SerializedName("DataTermino")
+    public Date dataTermino;
+
+    @SerializedName("Status")
+    public int status;
+
+
 }
