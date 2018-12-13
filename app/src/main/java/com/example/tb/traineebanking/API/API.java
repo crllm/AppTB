65b7b328b95d15b74b12ff523a5f83e0bf6b9bae
Allisson <allissonsampaio98@gmail.com>
2018-12-13 15:22:57 -0200

    asdasdasdas

--- app/src/main/java/com/example/tb/traineebanking/API/API.java
+++ app/src/main/java/com/example/tb/traineebanking/API/API.java
@@ -1,22 +1,11 @@
 package com.example.tb.traineebanking.API;
 
-import com.example.tb.traineebanking.Models.Acordo;
-import com.example.tb.traineebanking.Models.Boleto;
-import com.example.tb.traineebanking.Models.Conta;
-import com.example.tb.traineebanking.Models.Emprestimo;
-import com.example.tb.traineebanking.Models.Extrato;
-import com.example.tb.traineebanking.Models.Financiamento;
-import com.example.tb.traineebanking.Models.Investimento;
-import com.example.tb.traineebanking.Models.LogarConta;
-
-
-import java.util.HashMap;
+import com.example.tb.traineebanking.Models.*;
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
-import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
@@ -26,20 +15,11 @@ public interface API {
     @POST("/api/conta")
     Call<Conta>  verificarAcesso(@Body LogarConta logarConta);
 
-    @POST("api/extrato")
-    Call<List<Extrato>> getExtrato(@Body Conta conta);
-
-    @POST("api/emprestimo")
-    Call<List<Emprestimo>> getEmprestimos(@Body Conta conta);
-
     @POST("api/investimento")
-    Call<List<Investimento>> getInvestimentos(@Body Conta conta);
+    Call<Conta> gerarInvestimento(@Body Aplicacao aplicacao);
 
     @GET("api/investimento//{id}")
     Call<List<Investimento>> getInvestimentosId(@Path("id") int id);
-
-    @POST("api/boleto")
-    Call<List<Boleto>> getBoletos(@Body Conta conta);
 
     @PUT("/api/Boleto")
     Call<Boleto> pagarBoleto(@Body Boleto boleto);
@@ -70,4 +50,5 @@ public interface API {
 
     @PUT("/api/conta/{id}")
     Call<Conta>  alterarDadosCadastrais(@Path("id") int id, @Body Conta conta);
+
 }
