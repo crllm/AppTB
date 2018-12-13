7e0698aa2faa4d3b19db0b4c555b93dd662a0483
Allisson <allissonsampaio98@gmail.com>
2018-12-13 14:49:41 -0200

    asdfasdf

--- app/src/main/java/com/example/tb/traineebanking/API/API.java
+++ app/src/main/java/com/example/tb/traineebanking/API/API.java
@@ -1,9 +1,11 @@
 package com.example.tb.traineebanking.API;
 
+import com.example.tb.traineebanking.Models.Acordo;
 import com.example.tb.traineebanking.Models.Boleto;
 import com.example.tb.traineebanking.Models.Conta;
 import com.example.tb.traineebanking.Models.Emprestimo;
 import com.example.tb.traineebanking.Models.Extrato;
+import com.example.tb.traineebanking.Models.Financiamento;
 import com.example.tb.traineebanking.Models.Investimento;
 import com.example.tb.traineebanking.Models.LogarConta;
 
@@ -16,6 +18,7 @@ import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface API {
@@ -38,7 +41,33 @@ public interface API {
     @POST("api/boleto")
     Call<List<Boleto>> getBoletos(@Body Conta conta);
 
+    @PUT("/api/Boleto")
+    Call<Boleto> pagarBoleto(@Body Boleto boleto);
+
+    @PUT("/api/Conta")
+    Call<Conta> alterarConta(@Body Conta conta);
+
+    @GET("api/extrato/{id}")
+    Call<List<Extrato>> getExtrato(@Path("id") int id);
+
+    @POST("api/emprestimo")
+    Call<Emprestimo> gerarEmprestimo(@Body Financiamento financiamento);
+
+    @GET("api/listartodosinvestimentos//{id}")
+    Call<List<Investimento>> getTotosInvestimentos(@Path("id") int id);
+
+    @PUT("/api/investimento")
+    Call<Investimento> resgatarInvestimento(@Body Investimento investimento);
+
     @GET("api/boleto//{id}")
     Call<Boleto> getBoleto(@Path("id") int id);
 
+    @GET("api/acordo/{id}")
+    Call<List<Acordo>> getAcordos(@Path("id") int id);
+
+    @PUT("api/emprestimo")
+    Call<Emprestimo> gerarAcordo(@Body Acordo acordo);
+
+    @PUT("/api/conta/{id}")
+    Call<Conta>  alterarDadosCadastrais(@Path("id") int id, @Body Conta conta);
 }
