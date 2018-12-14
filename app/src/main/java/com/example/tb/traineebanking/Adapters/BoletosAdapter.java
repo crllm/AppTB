f9b732eb3c824e35951cac2045681287316cbca4
Allisson <allissonsampaio98@gmail.com>
2018-12-13 22:15:11 -0200

    PEGA ESSE

--- app/src/main/java/com/example/tb/traineebanking/Adapters/BoletosAdapter.java
+++ app/src/main/java/com/example/tb/traineebanking/Adapters/BoletosAdapter.java
@@ -49,7 +49,7 @@ public class BoletosAdapter extends RecyclerView.Adapter<BoletosAdapter.viewHold
 
         viewHolder.lblCodBoleto.setText("Código boleto: "+String.valueOf(boleto.getIdBoleto()));
         viewHolder.lblTipoBoleto.setText(boleto.getEmprestimo().getTipo());
-        viewHolder.lblValorParcela.setText("Valor: R$ " + Double.toString(boleto.getValor()));
+        viewHolder.lblValorParcela.setText("Valor: \nR$ " + Double.toString(boleto.getValor()));
         viewHolder.lblParcela.setText("Nº da Parcela: " + String.valueOf(boleto.getNumero()));
         viewHolder.lblStatusBoleto.setText("Status: " + String.valueOf(boleto.getStatus()));
     }
